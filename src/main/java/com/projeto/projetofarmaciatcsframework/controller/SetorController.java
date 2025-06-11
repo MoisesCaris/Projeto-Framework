aace1021f002f77092c66da38c418a3569cce18b
moisescaris <moises.caris@edu.unifil.br>
2025-06-11 15:27:34 -0300

    Adicionando controllers

---
+++ src/main/java/com/projeto/projetofarmaciatcsframework/controller/SetorController.java
@@ -0,0 +1,9 @@
+package com.projeto.projetofarmaciatcsframework.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/setor")
+public class SetorController {
+}
