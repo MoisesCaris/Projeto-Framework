b255dc7968a82a5e3610f24f80ce6fb8ccaaae97
Vinicius Augusto <viniciuscambe@hotmail.com>
2025-06-13 14:30:39 -0300

    Criação de Caixa e alteração do EstadoEnum

--- src/main/java/com/projeto/projetofarmaciatcsframework/service/CaixaService.java
+++ src/main/java/com/projeto/projetofarmaciatcsframework/service/CaixaService.java
@@ -1,6 +1,11 @@
 package com.projeto.projetofarmaciatcsframework.service;
 
-import com.projeto.projetofarmaciatcsframework.repository.FuncionarioRepository;
+import com.projeto.projetofarmaciatcsframework.DTO.caixa.RegistroCaixaDTO;
+import com.projeto.projetofarmaciatcsframework.mappers.CaixaMapper;
+import com.projeto.projetofarmaciatcsframework.models.CaixaModel;
+import com.projeto.projetofarmaciatcsframework.models.FarmaciaModel;
+import com.projeto.projetofarmaciatcsframework.repository.CaixaRepository;
+import com.projeto.projetofarmaciatcsframework.repository.FarmaciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +13,19 @@ import org.springframework.stereotype.Service;
 public class CaixaService {
 
     @Autowired
-    private FuncionarioRepository funcionarioRepository;
+    private CaixaRepository caixaRepository;
+
+    @Autowired
+    private FarmaciaRepository farmaciaRepository;
+
+    @Autowired
+    private CaixaMapper caixaMapper;
+
+    public CaixaModel registroCaixa(RegistroCaixaDTO dto, int idFarmacia) {
+       
+        FarmaciaModel farmacia = farmaciaRepository.findById(idFarmacia).get();
+        CaixaModel caixa = caixaMapper.registroCaixa(dto, farmacia);
+
+        return this.caixaRepository.save(caixa);
+    }
 }
