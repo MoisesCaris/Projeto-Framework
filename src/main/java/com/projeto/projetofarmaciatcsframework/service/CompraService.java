c95a5924ad8e4124f4dbd95706d996879ebe6bd0
moisescaris <moises.caris@edu.unifil.br>
2025-06-16 10:31:17 -0300

    Registro Compra protudos

--- src/main/java/com/projeto/projetofarmaciatcsframework/service/CompraService.java
+++ src/main/java/com/projeto/projetofarmaciatcsframework/service/CompraService.java
@@ -1,15 +1,10 @@
 package com.projeto.projetofarmaciatcsframework.service;
 
+import com.projeto.projetofarmaciatcsframework.DTO.compra.CompraProdutoDTO;
 import com.projeto.projetofarmaciatcsframework.DTO.venda.VendaRegistroDTO;
 import com.projeto.projetofarmaciatcsframework.mappers.CompraMapper;
-import com.projeto.projetofarmaciatcsframework.models.CompraModel;
-import com.projeto.projetofarmaciatcsframework.models.FarmaciaModel;
-import com.projeto.projetofarmaciatcsframework.models.FuncionarioModel;
-import com.projeto.projetofarmaciatcsframework.models.VendaModel;
-import com.projeto.projetofarmaciatcsframework.repository.CompraRepository;
-import com.projeto.projetofarmaciatcsframework.repository.ComprasProdutoRepository;
-import com.projeto.projetofarmaciatcsframework.repository.FarmaciaRepository;
-import com.projeto.projetofarmaciatcsframework.repository.FuncionarioRepository;
+import com.projeto.projetofarmaciatcsframework.models.*;
+import com.projeto.projetofarmaciatcsframework.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +20,8 @@ public class CompraService {
     @Autowired
     private CompraMapper mapper;
     @Autowired
+    private ProdutosRepository produtosRepository;
+    @Autowired
     private ComprasProdutoRepository comprasProdutoRepository;
 
     public void registrarVenda(VendaRegistroDTO data, Integer userId, Integer farmaciaID) {
@@ -34,7 +31,10 @@ public class CompraService {
         this.compraRepository.save(compraModel);
     }
 
-    public void registrarProdutos(){
-
+    public void registrarProdutos(CompraProdutoDTO data){
+        CompraModel compraModel = compraRepository.findById(data.idCompra()).orElseThrow();
+        ProdutosModel produtoModel = produtosRepository.findById(data.idProduto()).orElseThrow();
+        ComprasProdutoModel comprasProdutoModel = mapper.registroProduto(data,compraModel,produtoModel);
+        this.comprasProdutoRepository.save(comprasProdutoModel);
     }
 }
