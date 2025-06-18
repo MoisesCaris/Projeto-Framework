ed65e9523952d1561084f47ef29188e6f010a8df
moisescaris <moises.caris@edu.unifil.br>
2025-06-17 23:29:05 -0300

    Corrigido vendas futuras aparecendo no caixa

--- src/main/java/com/projeto/projetofarmaciatcsframework/repository/FarmaciaRepository.java
+++ src/main/java/com/projeto/projetofarmaciatcsframework/repository/FarmaciaRepository.java
@@ -16,15 +16,26 @@ public interface FarmaciaRepository extends JpaRepository<FarmaciaModel, Integer
     SELECT new com.projeto.projetofarmaciatcsframework.DTO.farmacia.FarmaciaCaixaDTO(
         f.nome,
         f.cnpj,
-        (f.caixaInicial
-         + COALESCE((SELECT SUM(v.totalVenda) FROM venda v WHERE v.farmacia = f), 0)
-         - COALESCE((SELECT SUM(c.totalCompra) FROM compra c WHERE c.farmacia = f), 0)
+        (
+            f.caixaInicial
+            + COALESCE((
+                SELECT SUM(v.totalVenda)
+                FROM venda v
+                WHERE v.farmacia = f
+                  AND v.dataVenda <= CURRENT_DATE
+            ), 0)
+            - COALESCE((
+                SELECT SUM(c.totalCompra)
+                FROM compra c
+                WHERE c.farmacia = f
+                  AND c.dataCompra <= CURRENT_DATE
+            ), 0)
         )
     )
     FROM farmacia f
     WHERE f.idFarmacia = :farmaciaId
 """)
-    FarmaciaCaixaDTO findSaldo(Integer farmaciaId);
+    FarmaciaCaixaDTO findSaldo(@Param("farmaciaId") Integer farmaciaId);
 
     @Query("""
     SELECT new com.projeto.projetofarmaciatcsframework.DTO.farmacia.LucroMensalDTO(
